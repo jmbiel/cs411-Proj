5542dc080c8d141acbc0cc7bfd661e334c4cc8f1
Max Biel <jmbiel@dhcp-wifi-8021x-155-41-8-215.bu.edu>
2018-04-25 17:20:41 -0400

    Added OAuth using twitter

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from urllib.parse import quote
 import requests
 from pymongo import MongoClient
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 
 """
 Yelp -- RELEVANT INFORMATION
@@ -10,20 +12,46 @@ Client ID: iQaMQisiPaQkU7QSSkzG3A
 API Key: H1ZaDuODlr5cjTjZ-yxUWt5us1U5VPdOXqB4f8We9-3Q1VQyt-jem1BTDCg4ZthQfLYsNF4TILRU6s2jQ8f93_tHrgEuKhoO5fatTBEdx6BrG-t9fM_3DotojmixWnYx
 """
 app = Flask(__name__)
+app.secret_key = "group5_secret_key"
 
 YELP_API_HOST = 'https://api.yelp.com'
 YELP_SEARCH_PATH = '/v3/businesses/search'
 YELP_API_KEY = 'H1ZaDuODlr5cjTjZ-yxUWt5us1U5VPdOXqB4f8We9-3Q1VQyt-jem1BTDCg4ZthQfLYsNF4TILRU6s2jQ8f93_tHrgEuKhoO5fatTBEdx6BrG-t9fM_3DotojmixWnYx'
+TWITTER_CLIENT_KEY = 'KGChrFAYY8eDwYuM2a0GmW85w'
+TWITTER_API_SECRET = 'UIAbnB0QF20eG2d0SFjVNET9Tu5emnrqi1rUz8lGy6eNTus8mP'
+TWITTER_ACCESS_TOKEN = '1223478032-PlhjfvY3Hg7b61xQeS7cWqdzgWb1hkzeMxgXU3I'
+TWITTER_ACCESS_SECRET = 'WDx05fPXl4h84u4D9UDlFdWHCdS8g1HqPkxoQbPIqffgs'
 
 # Setup Mongo DB connection
 client = MongoClient()
 repo = client["Nightlife_Reccomendation"]
 repo.authenticate('group5', 'group5')
 
+# Setup twitter OAuth
+twitter_blueprint = make_twitter_blueprint(api_key=TWITTER_CLIENT_KEY, api_secret=TWITTER_API_SECRET)
+app.register_blueprint(twitter_blueprint, url_prefix='/twitter_login')
 
 @app.route("/")
 def render_home():
-    return render_template("index.html")
+    if not twitter.authorized:
+        return redirect(url_for('twitter.login'))
+    
+    account_info = twitter.get('account/settings.json').json()
+    return render_template("index.html", user_name=account_info['screen_name'])
+
+@app.route("/twitter")
+def twitter_login():
+    if not twitter.authorized:
+        return redirect(url_for('twitter.login'))
+    account_info = twitter.get('account/settings.json')
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+
+        return '<h1>Your twitter name is @{}</h1>'.format(account_info_json['screen_name'])
+    
+    return "<h1>Request Failed</h1>"
+
 
 @app.route("/yelp_api_call", methods=['POST'])
 def yelp_api_call():
